48495d0643f31f91d24e00a9d713a275b61404ee
B <bkpratt84@gmail.com>
2017-08-28 22:14:01 -0500

    Move navigation to own component

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,39 +1,6 @@
 <template>
-    <v-app id="app" toolbar> 
-      <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow dark>
-        <v-toolbar flat class="transparent">
-          <v-list class="pa-0">
-            <v-list-tile avatar tag="div">
-              <v-list-tile-avatar>
-                <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>John Leider</v-list-tile-title>
-              </v-list-tile-content>
-              <v-list-tile-action>
-                <v-btn icon @click.native.stop="mini = !mini">
-                  <v-icon>chevron_left</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list>
-        </v-toolbar>
-        <v-list class="pt-0" dense>
-          <v-divider></v-divider>
-          <v-list-tile v-for="item in items" :key="item.title">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <v-toolbar fixed class="indigo darken-4" dark>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title>Wreaths Across America</v-toolbar-title>
-      </v-toolbar>
+    <v-app id="app"> 
+      <navigation></navigation>
       <main>
         <v-container fluid>
           <router-view></router-view>
@@ -43,23 +10,20 @@
 </template>
 
 <style>
-    /* div#root.application.application--light {
-        background-color: #ffffff;
-    } */
+
 </style>
 
 <script>
+  import Navigation from './Navigation.vue';
+
   export default {
     data () {
       return {
-        drawer: false,
-        items: [
-          { title: 'Home', icon: 'dashboard' },
-          { title: 'About', icon: 'question_answer' }
-        ],
-        mini: false,
-        right: null
+      
       }
+    },
+    components: {
+      navigation: Navigation
     }
   }
 </script>
---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,82 @@
+<template>
+    <v-app class="sidebar">
+        <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
+            <v-toolbar flat class="transparent">
+                <v-list class="pa-0">
+                <v-list-tile avatar tag="div">
+                    <v-list-tile-avatar>
+                    <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                    <v-list-tile-title>John Leider</v-list-tile-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                    <v-btn icon @click.native.stop="mini = !mini">
+                        <v-icon>chevron_left</v-icon>
+                    </v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+                </v-list>
+            </v-toolbar>
+
+            <v-list dense>
+                <template v-for="(item, i) in items">
+                    <v-layout
+                        row
+                        v-if="item.heading"
+                        align-center
+                        :key="i"
+                    >
+                        <v-flex xs6>
+                            <v-subheader v-if="item.heading">
+                                <strong>{{ item.heading }}</strong>
+                            </v-subheader>
+                        </v-flex>
+                    </v-layout>
+                    <v-divider
+                        dark
+                        v-else-if="item.divider"
+                        class="my-4"
+                        :key="i"
+                    ></v-divider>
+                    <v-list-tile
+                        :key="i"
+                        v-else
+                    >
+                        <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>
+                                {{ item.text }}
+                            </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+
+    <v-toolbar fixed class="indigo darken-4" dark>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Wreaths Across America</v-toolbar-title>
+      </v-toolbar> 
+    </v-app>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                items: [
+                    { icon: 'location_city', text: "Cemetery's" },
+                    { icon: 'people', text: 'Customers' },
+                    { icon: 'receipt', text: 'Orders' },
+                    { icon: 'local_hospital', text: 'Veterans' },
+                ],
+                drawer: true,
+                mini: false,
+                right: null
+            }
+        }
+    }
+</script>
